2824054f53266cc2a2f2cc2523bc5b531aa5eba3
Chirag <csoni111@gmail.com>
2016-12-09 04:04:48 +0530

    adds pagination for news articles

---
+++ assets/js/news.js
@@ -0,0 +1,50 @@
+var url = "https://iithbt.herokuapp.com/news/api/?page_size=25";
+var loading = true;
+var $loader_div = $("<div>", {"class": "post-box"});
+var $loader_img = $("<img>",{"src": "../images/loader.gif"});
+$loader_div.append($loader_img);
+var container;
+function loadNews() {
+    container.append($loader_div);
+    $.getJSON(url, function(json){
+        if (json.next != null) {
+            url = json.next;
+        } else {
+            $(window).off('scroll');
+        }
+        $loader_div.remove();
+        $.each(json.results, function(i, field){
+          console.log(i);
+          var tmpl = $.templates("#newsArticleTemplate");
+          var post = tmpl.render({post : field});
+          container.append(post);
+        });
+        loading=false;
+        // registerImageLoadingCallbacks();
+    });
+}
+function registerImageLoadingCallbacks() {
+    $('.post-image').each(function() {
+        $(this).imagesLoaded().done( function( instance ) {
+            console.log(instance);
+            container.waterfall({ 
+                colMinWidth: 300,
+                autoresize: true
+            });
+        });
+    });
+}
+
+$(document).ready(function() {
+    container = $(".waterfall");
+    loadNews();
+    $(window).on('scroll', function() {
+        if (!loading) {
+            if($(window).scrollTop() >= container.offset().top + container.outerHeight() - window.innerHeight) {
+                loading=true;
+                loadNews();
+                console.log('end reached');
+            }
+        }
+    });
+});
